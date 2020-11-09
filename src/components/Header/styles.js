3032bd8d48beab0c726b9cf3d915e71d707dac73
Daniel <daniel.kapexhiu@gmail.com>
2020-11-09 19:19:58 +0100

    new version

--- src/components/Header/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderLayout = styled.header`
-  text-align: center;
-
-  .app-logo {
-    width: 96px;
-  }
-`;
--- src/helpers.js
+++
@@ -1,5 +0,0 @@
-// Helper function to (dangerously) display markup.
-// https://facebook.github.io/react/docs/dom-elements.html#dangerouslysetinnerhtml
-export function createDangerousMarkup(props) {
-  return { __html: props };
-}
